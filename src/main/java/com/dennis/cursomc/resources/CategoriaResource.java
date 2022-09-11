da210a44ed84c4d0db70a7a23780b26bdb700643
dvaldrez <dennisvaldrez@hotmail.com>
2022-09-10 23:01:03 -0300

    criacao da classe service e repostiry categoria

--- src/main/java/com/dennis/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/dennis/cursomc/resources/CategoriaResource.java
@@ -3,25 +3,28 @@ package com.dennis.cursomc.resources;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.dennis.cursomc.domain.Categoria;
+import com.dennis.cursomc.services.CategoriaService;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 	
-	@RequestMapping(method=RequestMethod.GET)
-	public List<Categoria> lista() {
-		Categoria cat1 = new Categoria(1,"Informatica");
-		Categoria cat2 = new Categoria(2,"Escritório");
-		List<Categoria> lista = new ArrayList<>();		
-		lista.add(cat1);
-		lista.add(cat2);
-		
-		return lista;
+	@Autowired
+	private CategoriaService service;
+	//ResponseEntity encapsula varia info de uma resposta http para um serviço rest
+	@RequestMapping(value="/{id}",method=RequestMethod.GET)
+	public ResponseEntity<?> find(@PathVariable Integer id) {
+		Categoria obj = service.buscar(id);
+	
+		return ResponseEntity.ok().body(obj);
 	}
 
 }
---
+++ src/main/java/com/dennis/cursomc/services/CategoriaService.java
@@ -0,0 +1,26 @@
+package com.dennis.cursomc.services;
+
+import java.util.Optional;
+
+import org.hibernate.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.dennis.cursomc.domain.Categoria;
+import com.dennis.cursomc.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+	
+	//essa anotação instancia o objeto por injeçao de dependencia ou inversao de control
+	@Autowired
+	private CategoriaRepository repo;
+	
+	public Categoria buscar(Integer id) {
+		Optional<Categoria> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+		"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName(), null));
+		
+	}
+
+}
